b405200811fb8a9873bd27762969913071c2e41c
Maen Artimy <artimym@gmail.com>
2024-06-19 23:59:33 -0300

    add license

--- scripts/traffic_gen.py
+++ scripts/traffic_gen.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2024 Maen Artimy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from mininet.net import Mininet
 from mininet.log import setLogLevel, info
 import time
@@ -19,11 +43,11 @@ for h in host_names:
 server = hosts[3]
 
 # Start iperf3 server on h4
-info("*** Starting iperf3 server on h4\n")
+info("*** Starting iperf3 server\n")
 server.cmd("iperf3 -s &")
 
 # Start iperf3 clients on h1, h2, and h3 to send traffic to h4
-info("*** Starting iperf3 clients on h1, h2, and h3\n")
+info("*** Starting iperf3 clients\n")
 for h in hosts:
     h.cmd(
         f"iperf3 -c {server.IP()} -t {duration} -i 10 -P {streams} -b {bandwidth}M -M 1400 > /dev/null 2>&1 &"
